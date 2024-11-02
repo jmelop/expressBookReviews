ee15969ef6206e639f4e3c284a7872e2c65ec2d9
jmelop <jmelopdev@gmail.com>
2024-11-02 18:24:56 -0400

    feat: add authentication and new endpoint to get the customers

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,20 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided. Authorization denied.' });
+    }
+
+    jwt.verify(token, 'jwt_secret', (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: 'Token is not valid.' });
+        }
+
+        req.user = decoded;
+        next();
+    });
 });
  
 const PORT =5000;
